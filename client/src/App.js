e4e6dd904513185b3d0a2615fd97c6cb33b2baf4
Nicole Dong <xndong1020@gmail.com>
2025-02-16 16:22:58 +1100

    add BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import BookList from './components/BookList'
 function App() {
   return (
     <div id='main'>
       <h1>Ninja's Reading List</h1>
+      <BookList />
     </div>
   )
 }
