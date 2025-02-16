2ddb7e1e11e9668febe728894b38740a458316f3
Nicole Dong <xndong1020@gmail.com>
2025-02-16 16:23:22 +1100

    add BookList component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function BookList() {
+  return (
+    <div>
+      <ul id='book-list'>
+        <li>Book name</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList
